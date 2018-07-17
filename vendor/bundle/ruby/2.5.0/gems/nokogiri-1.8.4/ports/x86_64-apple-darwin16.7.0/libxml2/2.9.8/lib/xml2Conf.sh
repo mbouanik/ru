81c461f2f4b7e6d18b75d3b01703ae7b2c46a239
Matthis BOUANIK <mbouanik@e2r1p7.42.fr>
2018-07-17 15:49:28 +0200

    remove navbar from login page improve index page

---
+++ vendor/bundle/ruby/2.5.0/gems/nokogiri-1.8.4/ports/x86_64-apple-darwin16.7.0/libxml2/2.9.8/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/mbouanik/ru/vendor/bundle/ruby/2.5.0/gems/nokogiri-1.8.4/ports/x86_64-apple-darwin16.7.0/libxml2/2.9.8/lib"
+XML2_LIBS="-lxml2 -lz  -lpthread  -liconv  -lm "
+XML2_INCLUDEDIR="-I/Users/mbouanik/ru/vendor/bundle/ruby/2.5.0/gems/nokogiri-1.8.4/ports/x86_64-apple-darwin16.7.0/libxml2/2.9.8/include/libxml2"
+MODULE_VERSION="xml2-2.9.8"
+
