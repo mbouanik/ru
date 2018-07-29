c93d8b9e0760dcbae9bbf83f8e57da849c2df228
Matthis BOUANIK <mbouanik@e3r2p8.42.fr>
2018-07-29 14:03:08 +0200

    added location on the index profile and centered the picture

--- app/helpers/attendees_helper.rb
+++ app/helpers/attendees_helper.rb
@@ -4,26 +4,19 @@ require 'json'
 require 'thread'
 module AttendeesHelper
 
-  def get_pos(id)
-    # Thread.new {
-      client = OAuth2::Client.new( ENV["FT_ID"],  ENV["FT_SECRET"], site:"https://api.intra.42.fr")
-      token = client.client_credentials.get_token
-      info = token.get("/v2/users/" + id + "/locations/").parsed
-      location = info[0]["end_at"]
-      if location.nil?
-        location = info[0]['host']
-      else
-        location = "Unavailable"
-      end
-    return location
-    # }
-  end
-
-  # def location(login)
-  #   url = "https://profile.intra.42.fr/users/" + login
-  #   document = open(url)
-  #   content = document.read
-  #   parsed_content = Nokogiri::HTML(content)
-  #   print parsed_content.css('.content')
-
+	def get_pos(id)
+		client = OAuth2::Client.new( ENV["FT_ID"],  ENV["FT_SECRET"], site:"https://api.intra.42.fr")
+		token = client.client_credentials.get_token
+		info = token.get("/v2/users/" + id + "/locations/").parsed
+		# if info.status == 200
+			# info.parsed
+			location = info[0]["end_at"]
+			if location.nil?
+				location = info[0]['host']
+			else
+				location = "Unavailable"
+			end
+		return location
+		# end
+	end
 end
