eae27b943015bc7601443fa28ff3203fd1ddc461
liuleen <ruchenliu447@gmail.com>
2018-09-28 14:35:10 -0700

    please

---
+++ config/deploy.rb
@@ -0,0 +1,59 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+set :application, "h2s"
+set :repo_url, "https://github.com/mbouanik/ru.git"
+set :deploy_to, "/home/deployer/h2s"
+
+set :branch, "master"
+set :repository_cache, "git_cache"
+set :deploy_via, :remote_cache
+set :ssh_options, { :forward_agent => true }
+
+set :linked_files, %w{config/database.yml config/api_key.yml config/secrets.yml}
+set :linked_dirs, %w{log tmp/pids tmp/cache public/uploads}
+
+server "h2s-host.42.us.org", user: "deployer", roles: %w{app db web}, port: 4222
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
+set :rbenv_roles, :all # default value
+
+after 'deploy:publishing', 'deploy:restart'
+
+set :passenger_restart_with_touch, true
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
