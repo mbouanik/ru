a074a3e361a0359a2662c9b5cb8dc86ef1f8450e
liuleen <ruchenliu447@gmail.com>
2018-10-02 16:01:00 -0700

    change port

--- shared/puma.rb
+++ shared/puma.rb
@@ -7,7 +7,7 @@ state_path "/home/deployer/h2s/shared/tmp/pids/puma.state"
 stdout_redirect '/home/deployer/h2s/shared/log/puma_access.log', '/home/deployer/h2s/shared/log/puma_error.log', true
 threads 0,16
 #bind 'unix:///home/deployer/h2s/shared/tmp/sockets/puma.sock'
-port 8080
+port 80
 workers 0
 prune_bundler
 on_restart do
